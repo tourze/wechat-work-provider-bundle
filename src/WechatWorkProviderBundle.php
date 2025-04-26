323d70127d0561c493dff771fc14bd8baa92b5d8
github-ci <you@example.com>
2025-04-26 08:08:56 +0000

    refactor(wechat-work-provider-bundle): 移除依赖接口并更新 composer.json

--- src/WechatWorkProviderBundle.php
+++ src/WechatWorkProviderBundle.php
@@ -3,17 +3,9 @@
 namespace WechatWorkProviderBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
 #[AsPermission(title: '企业微信服务商')]
-class WechatWorkProviderBundle extends Bundle implements BundleDependencyInterface
+class WechatWorkProviderBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatWorkBundle\WechatWorkBundle::class => ['all' => true],
-            \WechatWorkExternalContactBundle\WechatWorkExternalContactBundle::class => ['all' => true],
-        ];
-    }
 }
